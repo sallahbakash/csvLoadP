44a7ec0be79469636f04a240c64ee217dc2ebfea
sallahbakash <sallahbakash@gmail.com>
2024-02-08 19:33:19 +0530

    Update tuto_chatbot_csv.py

--- tuto_chatbot_csv.py
+++ tuto_chatbot_csv.py
@@ -12,7 +12,7 @@ import tempfile
 
 user_api_key = st.sidebar.text_input(
     label="#### Your OpenAI API key 👇",
-    placeholder="Paste your openAI API key, sk-",
+    placeholder="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",
     type="password")
 
 uploaded_file = st.sidebar.file_uploader("upload", type="csv")
